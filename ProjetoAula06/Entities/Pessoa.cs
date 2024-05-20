1e0c7c4fde09433a0fc7562abf610e04a98cd3e1
EsterRibeiro <ester.ribeiro10101@gmail.com>
2024-05-20 13:04:55 -0300

    first commit

---
+++ ProjetoAula06/Entities/Pessoa.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ProjetoAula06.Entities;
+/// <summary>
+/// Modelo de entidade
+/// </summary>
+public class Pessoa
+{
+    #region Atributos
+    private Guid _id;
+    private string _nome;
+    private string _cpf;
+    #endregion
+    public int MyProperty { get; set; }
+
+    #region Propriedades
+    public Guid Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+
+    public string Nome 
+    {
+        get { return _nome; }
+        set {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException("O nome da pessoa é obrigatório");
+
+            var regex = new Regex("^[A-Za-zÀ-Üà-ü\\s]{8,100}$"); //permite de A a Z maiúsculo e minúsculo, acentos, espaços e entre 8 a 100 caracteres
+
+            if (!regex.IsMatch(value))
+                throw new ArgumentException("O nome da pessoa deve ter somente texto de 8 a 100 caracteres");
+
+            _nome = value; 
+        }     
+    }
+
+    public string Cpf
+    {
+        get { return _cpf; }
+        set {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException("O cpf é obrigatório.");
+
+            var regex = new Regex("^[0-9]{11}$");
+            if (!regex.IsMatch(value))
+                throw new ArgumentException("O cpf da pessoa deve ter exatamente 11 dígitos (sem símbolos.)");
+            _cpf = value; }
+    }
+
+    #endregion
+}
---
+++ ProjetoAula06/Program.cs
@@ -0,0 +1,48 @@
+﻿using ProjetoAula06.Entities;
+
+namespace ProjetoAula06;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Cadastro de pessoa: ");
+
+        try
+        {
+
+            var pessoa = new Pessoa();
+            pessoa.Id = Guid.NewGuid();
+
+            Console.Write("Nome da pessoa: ");
+            pessoa.Nome = Console.ReadLine();
+
+            Console.Write("CPF: ");
+            pessoa.Cpf = Console.ReadLine();
+
+            Console.WriteLine("Dados de pessoa:");
+            Console.WriteLine($"id: {pessoa.Id}");
+            Console.WriteLine($"nome: {pessoa.Nome}");
+            Console.WriteLine($"cpf: {pessoa.Cpf}");
+
+
+        }
+        catch (ArgumentException e) {
+            Console.WriteLine("\nErro de validação");
+            Console.WriteLine(e.Message);
+        }
+        finally {
+            Console.Write("Deseja repetir o processo? S/N: "); //ocorre independente de cair no try ou catch, não é obrigatório
+            var opcao = Console.ReadLine();
+
+            if (opcao.Equals("S", StringComparison.OrdinalIgnoreCase)) { 
+                Console.Clear();
+                Main(args);
+            }
+            else
+            {
+                Console.WriteLine("Fim do programa.");
+            }
+        }
+    }
+}
